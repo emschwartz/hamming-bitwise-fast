7aed0c31893c29742bc19ddc73bf2eead083ae30
Evan Schwartz <3262610+emschwartz@users.noreply.github.com>
2024-12-19 17:43:48 -0500

    Speed up naive implementations

--- src/naive.rs
+++ src/naive.rs
@@ -1,15 +1,17 @@
+#[inline]
 pub fn naive_hamming_distance(x: &[u8], y: &[u8]) -> u64 {
     assert_eq!(x.len(), y.len());
-    let mut distance = 0;
+    let mut distance: u32 = 0;
     for i in 0..x.len() {
-        distance += (x[i] ^ y[i]).count_ones() as u64;
+        distance += (x[i] ^ y[i]).count_ones();
     }
-    distance
+    distance as u64
 }
 
+#[inline]
 pub fn naive_hamming_distance_iter(x: &[u8], y: &[u8]) -> u64 {
     assert_eq!(x.len(), y.len());
     x.iter()
         .zip(y)
-        .fold(0, |a, (b, c)| a + (*b ^ *c).count_ones() as u64)
+        .fold(0, |a, (b, c)| a + (*b ^ *c).count_ones()) as u64
 }
