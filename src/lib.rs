342fb3f240d2f1010bd6ba2530f14d0a04f6f18b
Evan Schwartz <3262610+emschwartz@users.noreply.github.com>
2024-12-20 08:29:35 -0500

    Move implementations to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,47 @@
-mod naive;
+#[inline]
+pub fn naive_hamming_distance(x: &[u8], y: &[u8]) -> u64 {
+    assert_eq!(x.len(), y.len());
+    let mut distance: u32 = 0;
+    for i in 0..x.len() {
+        distance += (x[i] ^ y[i]).count_ones();
+    }
+    distance as u64
+}
 
-pub use naive::*;
+#[inline]
+pub fn naive_hamming_distance_iter(x: &[u8], y: &[u8]) -> u64 {
+    x.iter()
+        .zip(y)
+        .fold(0, |a, (b, c)| a + (*b ^ *c).count_ones()) as u64
+}
+
+#[inline]
+pub fn hamming_distance_auto_vectorized(x: &[u8], y: &[u8]) -> u64 {
+    assert_eq!(x.len(), y.len());
+
+    // Process 8 bytes at a time using u64
+    let mut distance = x
+        .chunks_exact(8)
+        .zip(y.chunks_exact(8))
+        .map(|(x_chunk, y_chunk)| {
+            let x_val = u64::from_ne_bytes(x_chunk.try_into().unwrap());
+            let y_val = u64::from_ne_bytes(y_chunk.try_into().unwrap());
+            (x_val ^ y_val).count_ones()
+        })
+        .sum::<u32>();
+
+    if x.len() % 8 != 0 {
+        distance += x
+            .chunks_exact(8)
+            .remainder()
+            .iter()
+            .zip(y.chunks_exact(8).remainder())
+            .map(|(x_byte, y_byte)| (x_byte ^ y_byte).count_ones())
+            .sum::<u32>();
+    }
+
+    distance as u64
+}
 
 #[test]
 fn all_same_results() {
